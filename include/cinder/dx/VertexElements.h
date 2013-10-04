4a35be1002919946c7799544f4a22e6e076d3564
mrvux <julien@mrvux.com>
2013-10-04 13:02:00 +0200

    -Added semanic index to color -Added TexCoord3 and TexCoord4

--- include/cinder/dx/VertexElements.h
+++ include/cinder/dx/VertexElements.h
@@ -11,12 +11,14 @@ public:
 	static D3D11_INPUT_ELEMENT_DESC Position3(int slot = 0);
 	static D3D11_INPUT_ELEMENT_DESC Position4(int slot = 0);
 
-	static D3D11_INPUT_ELEMENT_DESC Color3(int slot = 0);
-	static D3D11_INPUT_ELEMENT_DESC Color4(int slot = 0);
-
 	static D3D11_INPUT_ELEMENT_DESC Normal3(int slot = 0);
 
+	static D3D11_INPUT_ELEMENT_DESC Color3(int slot = 0, int index = 0);
+	static D3D11_INPUT_ELEMENT_DESC Color4(int slot = 0, int index = 0);
+
 	static D3D11_INPUT_ELEMENT_DESC TexCoord2(int slot = 0,int index = 0);
+	static D3D11_INPUT_ELEMENT_DESC TexCoord3(int slot = 0,int index = 0);
+	static D3D11_INPUT_ELEMENT_DESC TexCoord4(int slot = 0,int index = 0);
 };
 
 } }
